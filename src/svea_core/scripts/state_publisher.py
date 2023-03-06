19673d0a8152f30cef71441b8bf4827cd70999d3
Fedezac <fedezac99@gmail.com>
2023-03-06 09:48:32 +0100

    Fixed state publisher

--- src/svea_core/scripts/state_publisher.py
+++ src/svea_core/scripts/state_publisher.py
@@ -28,7 +28,7 @@ class state_publisher:
         ## Create node resources
 
         # spin rate
-        self.rate = rospy.Rate(SPIN_RATE)
+        self.rate = rospy.Rate(self.SPIN_RATE)
 
         # Visualization tool
         self.rviz = RVIZPathHandler()
@@ -49,7 +49,7 @@ class state_publisher:
             lambda msg: self.log('Clikcked point: [%f, %f]', msg.x, msg.y)
         )
 
-        self.loginfo("init done")
+        rospy.loginfo("init done")
 
     def run(self):
         while self.keep_alive():
@@ -60,7 +60,7 @@ class state_publisher:
         return not rospy.is_shutdown()
 
     def spin(self):
-        self.log('[%f, %f, %f]', self.state.x, self.state.y, self.state.yaw)
+        rospy.loginfo('[%f, %f, %f]', self.state.x, self.state.y, self.state.yaw)
         self.rviz.log_state(self.state)
         self.rviz.visualize_data()
 
